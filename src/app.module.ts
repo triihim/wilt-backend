93996c8f647160ac29edcf6bda3bbfe807db03cb
triihim <tatu.riihimaki@gmail.com>
2023-11-15 17:47:26 +0000

    added config validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmConfigService } from './config/typeorm.config';
 import { AuthModule } from './auth/auth.module';
+import { validateConfig } from './config/config-validation';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: ['./env/development.env'],
+      envFilePath: './env/development.env',
+      validate: validateConfig,
     }),
     TypeOrmModule.forRootAsync({
       useClass: TypeOrmConfigService,
---
+++ src/config/config-validation.ts
@@ -0,0 +1,52 @@
+import { plainToClass } from 'class-transformer';
+import { IsDefined, validateSync } from 'class-validator';
+
+class EnvironmentVariables {
+  @IsDefined()
+  DB_TYPE: string;
+
+  @IsDefined()
+  DB_HOST: string;
+
+  @IsDefined()
+  DB_PORT: number;
+
+  @IsDefined()
+  DB_USERNAME: string;
+
+  @IsDefined()
+  DB_PASSWORD: string;
+
+  @IsDefined()
+  DB_NAME: string;
+
+  @IsDefined()
+  SALT_ROUNDS: number;
+
+  @IsDefined()
+  TOKEN_ALGORITHM: string;
+
+  @IsDefined()
+  TOKEN_SECRET: string;
+
+  @IsDefined()
+  TOKEN_EXPIRES_IN: string;
+
+  @IsDefined()
+  REFRESH_TOKEN_EXPIRES_IN: string;
+}
+
+export function validateConfig(configuration: Record<string, unknown>) {
+  const finalConfig = plainToClass(EnvironmentVariables, configuration, {
+    enableImplicitConversion: true,
+  });
+
+  const errors = validateSync(finalConfig, { skipMissingProperties: false });
+
+  if (errors.length) {
+    console.error(errors);
+    throw new Error('Missing environment variables');
+  }
+
+  return finalConfig;
+}
