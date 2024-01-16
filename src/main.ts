456e28ffe920a11298979f0e67b77bcbbe27289a
triihim <tatu.riihimaki@gmail.com>
2024-01-16 17:23:14 +0000

    changed port from 3000 to 8000 and enabled cors with origin *

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+
+  app.enableCors({
+    origin: '*',
+  });
+
+  await app.listen(8000);
 }
 bootstrap();
